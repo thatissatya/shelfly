b5f0c73fd0d408f7dab4fd5d52af089c86d7565d
Satya Prakash <satyaprakashksingh@gmail.com>
2023-03-10 22:38:54 +0530

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, 'src', 'index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: path.resolve(__dirname, 'node_modules'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html')
+        })
+    ],
+    mode: 'development'
+}
